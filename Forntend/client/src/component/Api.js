b9774acac80e45917c7b61ccb497b2049a8b0643
Sai Krishna <saikrishnajv22@gmail.com>
2025-02-05 18:39:45 +0530

    updated Api

--- Forntend/client/src/component/Api.js
+++ Forntend/client/src/component/Api.js
@@ -1,3 +1,3 @@
 //  export const Api = "http://localhost:4000"
 
-export const Api = "https://backend-e-commerce-iacw.onrender.com"
+export const Api = "https://e-commerce-backend-lb0p.onrender.com"
