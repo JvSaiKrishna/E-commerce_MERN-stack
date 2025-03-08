d8ba6e570becef738e8ded610f760a3907093ca2
Sai Krishna <saikrishnajv22@gmail.com>
2025-03-08 19:59:28 +0530

    Added loader and show password

--- Frontend/client/src/component/Vendor/VendorForms/Login.js
+++ Frontend/client/src/component/Vendor/VendorForms/Login.js
@@ -3,6 +3,7 @@ import Cookies from "js-cookie"
 import { Navigate, useNavigate } from 'react-router-dom'
 import Navbar from '../Navbar'
 import { Api } from '../../Api'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 // dotEnv.config()
@@ -12,25 +13,41 @@ const VendorLogin = () => {
     const [username, setUsername] = useState("")
     const [password, setpassword] = useState("")
     const [errMsg, setErrMsg] = useState('')
+    const [userText, setUserText] = useState('')
+    const [passwordText, setPasswordText] = useState('')
+    const [isLogFailed, setisLogFailed] = useState(false)
+    const [showPwd, setShowPwd] = useState(true)
+
 
     const navigate = useNavigate()
-    
+
     const jwt = Cookies.get("jwt_token")
     // console.log(jwt)
-    if(jwt){
-        return <Navigate to="/Shopinity/vendor" replace/>
+    if (jwt) {
+        return <Navigate to="/Shopinity/vendor" replace />
     }
 
     const onChangeUsername = (event) => {
         setErrMsg("")
+        setUserText("")
         setUsername(event.target.value)
     }
     const onChangePassword = (event) => {
-        // setErrMsg("")
+        setErrMsg("")
+        setPasswordText("")
         setpassword(event.target.value)
     }
     const SubmitHandler = async (event) => {
         event.preventDefault();
+        setisLogFailed(true)
+        if (!username) {
+            setisLogFailed(false)
+            return setUserText("Required")
+        }
+        if (!password) {
+            setisLogFailed(false)
+            return setPasswordText("Required")
+        }
         const Data = { username, password }
         const options = {
             method: "POST",
@@ -44,17 +61,18 @@ const VendorLogin = () => {
         const response = await fetch(url, options)
         const data = await response.json()
         if (response.ok) {
-            Cookies.set("jwt_token",data.getJwt,{expires:30})
+            Cookies.set("jwt_token", data.getJwt, { expires: 30 })
             alert("Login Success")
-            navigate("/Shopinity/vendor" , {replace:true})
+            navigate("/Shopinity/vendor", { replace: true })
         }
         else {
             // console.log(data)
             setErrMsg(data)
-            
+
         }
         setUsername('')
         setpassword("")
+        setisLogFailed(false)
         // setErrMsg("")
 
 
@@ -64,20 +82,31 @@ const VendorLogin = () => {
 
     return (
         <>
-        <Navbar/>
+            <Navbar />
             <form className='sign-form' onSubmit={SubmitHandler}>
                 <h2>Login</h2>
                 <div>
                     <label htmlFor='username'>Username</label>
                     <input value={username} onChange={onChangeUsername} type='text' id='username' placeholder='Username' />
                 </div>
+                {userText && <p style={{ color: "red" }}>*{userText}</p>}
 
                 <div>
                     <label htmlFor='password'>Password</label>
-                    <input value={password} onChange={onChangePassword} type='password' id='password' placeholder='Password' />
+                    <div >
+
+                        <input type={showPwd ? 'password' : "text"} value={password} onChange={onChangePassword} id='password' placeholder='Password' />
+                        <button onClick={
+                            (e) => {
+                                e.preventDefault()
+                                setShowPwd(!showPwd)
+                            }
+                        } >{showPwd ? <FaEye className='eye-logo' /> : <FaEyeSlash className='eye-logo' />}</button>
+                    </div>
                 </div>
-                <button type='submit'>Submit</button>
-                {errMsg&&<p style={{color:"red"}}>*{errMsg}</p>}
+                {passwordText && <p style={{ color: "red" }}>*{passwordText}</p>}
+                <button className = {`submit ${isLogFailed ?"btns-loading":"btns-not-loading"}`} disabled = {isLogFailed} type='submit' >{isLogFailed ? <span className="loaders"></span> : "Login"}</button>
+                {errMsg && <p style={{ color: "red" }}>*{errMsg}</p>}
             </form>
         </>
     )
--- Frontend/client/src/component/Vendor/VendorForms/Registration.js
+++ Frontend/client/src/component/Vendor/VendorForms/Registration.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import Navbar from '../Navbar'
 import { Api } from '../../Api'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 // import "./App.css"
@@ -12,52 +13,77 @@ export const VendorRegistration = () => {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+  const [userText, setUserText] = useState('')
+  const [emailText, setEmailText] = useState('')
+  const [passwordText, setPasswordText] = useState('')
+  const [isLogFailed, setisLogFailed] = useState(false)
+  const [showPwd, setShowPwd] = useState(true)
 
 
   const onChangeUsername = (event) => {
+    setErrMsg("")
+    setUserText("")
     setUsername(event.target.value)
   }
   const onChangePassword = (event) => {
-
+    setErrMsg("")
+    setPasswordText("")
     setPassword(event.target.value)
   }
   const onChangeEmail = (event) => {
+    setErrMsg("")
+    setEmailText("")
     setEmail(event.target.value)
   }
   const SubmitHandler = async (event) => {
     event.preventDefault();
-    if(password.length >= 6){
-      const Data = { username, password, email }
-    const options = {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(Data)
-
+    setisLogFailed(true)
+    if (!username) {
+      setisLogFailed(false)
+      return setUserText("Required")
     }
-    const url = `${Api}/Shopinity/vendor/registration`
-    // console.log(Data)
-    const response = await fetch(url, options)
-    const data = await response.json()
-    if (response.ok) {
-      alert(data)
-      return Navigate('/Shopinity/vendor/login', { replace: true })
+    if (!email) {
+      setisLogFailed(false)
+      return setEmailText("Required")
+    }
+    if (!password) {
+      setisLogFailed(false)
+      return setPasswordText("Required")
+    }
+    if (password.length >= 6) {
+      const Data = { username, password, email }
+      const options = {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Data)
+
+      }
+      const url = `${Api}/Shopinity/vendor/registration`
+      // console.log(Data)
+      const response = await fetch(url, options)
+      const data = await response.json()
+      if (response.ok) {
+        alert(data)
+        return Navigate('/Shopinity/vendor/login', { replace: true })
+
+      }
+      else {
+        setErrMsg(data)
+
+      }
 
     }
     else {
-      alert(data)
-
-    }
-
-    }
-    else{
       alert("Password must be atleast 6 characters long")
     }
-    
+
     setEmail('')
     setPassword('')
     setUsername('')
+    setisLogFailed(false)
   }
 
   return (
@@ -69,15 +95,27 @@ export const VendorRegistration = () => {
           <label htmlFor='username'>Username</label>
           <input value={username} onChange={onChangeUsername} type='text' id='username' placeholder='Username' />
         </div>
+        {userText && <p style={{ color: "red" }}>*{userText}</p>}
         <div>
           <label htmlFor='email'>Email</label>
-          <input value={email} onChange={onChangeEmail} type='text' id='email' placeholder='Email' />
+          <input value={email} onChange={onChangeEmail} type='email' id='email' placeholder='Email' />
         </div>
+        {emailText && <p style={{ color: "red" }}>*{emailText}</p>}
         <div>
           <label htmlFor='password'>Password</label>
-          <input value={password} onChange={onChangePassword} type='password' id='password' placeholder='Password' />
+          <div>
+            <input type={showPwd ? 'password' : "text"} value={password} onChange={onChangePassword} id='password' placeholder='Password' />
+            <button onClick={
+              (e) => {
+                e.preventDefault()
+                setShowPwd(!showPwd)
+              }
+            } >{showPwd ? <FaEye className='eye-logo' /> : <FaEyeSlash className='eye-logo' />}</button>
+          </div>
         </div>
-        <button>Submit</button>
+        {passwordText && <p style={{ color: "red" }}>*{passwordText}</p>}
+        <button disabled = {isLogFailed} className = {`submit ${isLogFailed ?"btns-loading":"btns-not-loading"}`}>{isLogFailed ? <span className="loaders"></span> : "Sign Up"}</button>
+        {errMsg && <p style={{ color: "red" }}>*{errMsg}</p>}
       </form>
     </>
   )
