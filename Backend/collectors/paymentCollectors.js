5eb57136705c431d944a4ee6378a89949c837665
Sai Krishna <saikrishnajv22@gmail.com>
2025-03-28 15:23:39 +0530

    Added order creation logic in verify payment function and linked cart products to the order

--- Backend/collectors/paymentCollectors.js
+++ Backend/collectors/paymentCollectors.js
@@ -1,6 +1,8 @@
 import Razorpay from "razorpay"
 import dotEnv from "dotenv"
 import crypto from "crypto"
+import order from "../models/order.js"
+import { cart } from "../models/Cart.js"
 
 
 dotEnv.config()
@@ -38,6 +40,18 @@ const verifyPayment = async (req, res) => {
         .digest("hex");
 
     if (generated_signature === razorpay_signature) {
+        const newOrder = new order({
+            orderId: razorpay_order_id,
+            paymentId: razorpay_payment_id,
+            status: "Paid"
+        })
+        await newOrder.save()
+        const savedOrder = await order.findOne({ orderId: razorpay_order_id })
+        const products = await cart.find({ userId: req.userId })
+        for (let i = 0; i < products?.length; i++) {
+            savedOrder.productId.push(products[i].productId)
+        }
+        await savedOrder.save()
         res.status(200).json({ message: "Payment verified successfully", payment_id: razorpay_payment_id });
     } else {
         res.status(400).json({ message: "Payment verification failed" });
