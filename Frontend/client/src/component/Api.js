2c74b88811a13dfcb27122710e652a9940a638ee
Sai Krishna <saikrishnajv22@gmail.com>
2025-09-08 18:21:06 +0530

    Updated Backend url

--- Frontend/client/src/component/Api.js
+++ Frontend/client/src/component/Api.js
@@ -1,5 +1,6 @@
 //  export const Api = "http://localhost:4000"
 
 
-export const Api = "https://e-commerce-backend-lb0p.onrender.com"
+// export const Api = "https://e-commerce-backend-lb0p.onrender.com"
+export const Api = "https://e-commerce-5v0v.onrender.com"
 
