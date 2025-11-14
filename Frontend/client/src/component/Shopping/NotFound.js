b960147caad922a3b019c8b4496014c1198d8c07
Sai Krishna <saikrishnajv22@gmail.com>
2025-11-14 14:20:25 +0530

    Added 'shop now' button

--- Frontend/client/src/component/Shopping/NotFound.js
+++ Frontend/client/src/component/Shopping/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
     <div className='not-found-container'>
-      <img src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png" alt="not found" className='not-found-img'/>
+      <img src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png" alt="not found" className='not-found-img' />
+      <Link to="/Shopinity/products">
+        <button type="button" className="shop-now-button">Shop Now</button>
+      </Link>
     </div>
   )
 }
